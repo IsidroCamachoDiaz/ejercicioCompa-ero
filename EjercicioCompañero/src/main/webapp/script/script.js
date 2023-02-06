c4255166a824554d0c9e65e714ae9514c4900aab
DESKTOP-5AFE429\Isidro Camacho <isidroswicth@gmail.com>
2023-02-06 08:54:14 +0100

    EjercicioHecho

---
+++ EjercicioCompañero/src/main/webapp/script/script.js
@@ -0,0 +1,12 @@
+function tipo1(){
+	let ejemplo1=true;
+	document.write(typeof(ejemplo1));
+}
+function tipo2(){
+	let ejemplo2=85.4
+	document.write(typeof(ejemplo2));
+}
+function tipo3(){
+	let ejemplo3=12
+	document.write(typeof(ejemplo3));
+}
